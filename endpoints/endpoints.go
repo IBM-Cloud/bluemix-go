333abcad48b7d8e7149cae47231e35e3dd3c67bd
STEPHEN STRUTT <steve_strutt@uk.ibm.com>
2019-02-06 17:40:35 +0000

    additional icd endpoints

--- endpoints/endpoints.go
+++ endpoints/endpoints.go
@@ -92,7 +92,9 @@ var regionToEndpoint = map[string]map[string]string{
 	},
 	"icd": {
 		"us-south": "https://api.us-south.databases.cloud.ibm.com",
+		"us-east": "https://api.us-east.databases.cloud.ibm.com",
 		"eu-de":    "https://api.eu-de.databases.cloud.ibm.com",
+		"eu-gb":    "https://api.eu-gb.databases.cloud.ibm.com",
 		"au-syd":   "https://api.au-syd.databases.cloud.ibm.com",
 		"jp-tok":   "https://api.jp-tok.databases.cloud.ibm.com",
 		"oslo01": 	"https://api.osl01.databases.cloud.ibm.com",
