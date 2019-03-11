c728f45f09c472d07fe10eacd481d10084d5e753
STEPHEN STRUTT <steve_strutt@uk.ibm.com>
2019-03-11 15:17:29 +0000

    global tagging

--- endpoints/endpoints_test.go
+++ endpoints/endpoints_test.go
@@ -18,6 +18,14 @@ var _ = Describe("EndPoints", func() {
 			Expect(locator.ContainerEndpoint()).To(Equal("https://containers.bluemix.net"))
 			Expect(locator.CisEndpoint()).To(Equal("https://api.cis.cloud.ibm.com"))
 			Expect(locator.ICDEndpoint()).To(Equal("https://api.us-south.databases.cloud.ibm.com"))
+			Expect(locator.GlobalSearchEndpoint()).To(Equal("https://api.global-search-tagging.cloud.ibm.com"))
+			Expect(locator.GlobalTaggingEndpoint()).To(Equal("https://tags.global-search-tagging.cloud.ibm.com"))
+			Expect(locator.MCCPAPIEndpoint()).To(Equal("https://mccp.ng.bluemix.net")) 
+			Expect(locator.IAMPAPEndpoint()).To(Equal("https://iam.bluemix.net")) 
+			Expect(locator.ResourceManagementEndpoint()).To(Equal("https://resource-manager.bluemix.net")) 
+			Expect(locator.ResourceControllerEndpoint()).To(Equal("https://resource-controller.bluemix.net")) 
+			Expect(locator.ResourceCatalogEndpoint()).To(Equal("https://resource-catalog.bluemix.net")) 
+			Expect(locator.ContainerRegistryEndpoint()).To(Equal("https://registry.ng.bluemix.net")) 
 		})
 	})
 
@@ -30,6 +38,9 @@ var _ = Describe("EndPoints", func() {
 			Expect(locator.AccountManagementEndpoint()).To(Equal("https://accountmanagement.eu-gb.bluemix.net"))
 			Expect(locator.IAMEndpoint()).To(Equal("https://iam.bluemix.net"))
 			Expect(locator.CisEndpoint()).To(Equal("https://api.cis.cloud.ibm.com"))
+			Expect(locator.ICDEndpoint()).To(Equal("https://api.eu-gb.databases.cloud.ibm.com"))
+			Expect(locator.GlobalSearchEndpoint()).To(Equal("https://api.global-search-tagging.cloud.ibm.com"))
+			Expect(locator.GlobalTaggingEndpoint()).To(Equal("https://tags.global-search-tagging.cloud.ibm.com"))
 		})
 	})
 
@@ -42,6 +53,7 @@ var _ = Describe("EndPoints", func() {
 			Expect(locator.AccountManagementEndpoint()).To(Equal("https://accountmanagement.au-syd.bluemix.net"))
 			Expect(locator.IAMEndpoint()).To(Equal("https://iam.bluemix.net"))
 			Expect(locator.CisEndpoint()).To(Equal("https://api.cis.cloud.ibm.com"))
+			Expect(locator.GlobalTaggingEndpoint()).To(Equal("https://tags.global-search-tagging.cloud.ibm.com"))
 			Expect(locator.ICDEndpoint()).To(Equal("https://api.au-syd.databases.cloud.ibm.com"))
 		})
 	})
@@ -55,6 +67,7 @@ var _ = Describe("EndPoints", func() {
 			Expect(locator.AccountManagementEndpoint()).To(Equal("https://accountmanagement.eu-de.bluemix.net"))
 			Expect(locator.IAMEndpoint()).To(Equal("https://iam.bluemix.net"))
 			Expect(locator.CisEndpoint()).To(Equal("https://api.cis.cloud.ibm.com"))
+			Expect(locator.GlobalTaggingEndpoint()).To(Equal("https://tags.global-search-tagging.cloud.ibm.com"))
 			Expect(locator.ICDEndpoint()).To(Equal("https://api.eu-de.databases.cloud.ibm.com"))
 		})
 	})
@@ -68,8 +81,27 @@ var _ = Describe("EndPoints", func() {
 			Expect(locator.AccountManagementEndpoint()).To(Equal("https://accountmanagement.us-east.bluemix.net"))
 			Expect(locator.IAMEndpoint()).To(Equal("https://iam.bluemix.net"))
 			Expect(locator.CisEndpoint()).To(Equal("https://api.cis.cloud.ibm.com"))
+			Expect(locator.GlobalTaggingEndpoint()).To(Equal("https://tags.global-search-tagging.cloud.ibm.com"))
+			Expect(locator.ICDEndpoint()).To(Equal("https://api.us-east.databases.cloud.ibm.com"))
 		})
 	})
+
+	Context("When region is jp-tok", func() {
+		locator := newEndpointLocator("jp-tok")
+
+		It("should return endpoints with region jp-tok", func() {
+			Expect(locator.CFAPIEndpoint()).To(Equal("https://api.jp-tok.bluemix.net"))
+			Expect(locator.UAAEndpoint()).To(Equal("https://login.jp-tok.bluemix.net/UAALoginServerWAR"))
+			Expect(locator.AccountManagementEndpoint()).To(Equal("https://accountmanagement.jp-tok.bluemix.net"))
+			Expect(locator.IAMEndpoint()).To(Equal("https://iam.bluemix.net"))
+			Expect(locator.CisEndpoint()).To(Equal("https://api.cis.cloud.ibm.com"))
+			Expect(locator.GlobalTaggingEndpoint()).To(Equal("https://tags.global-search-tagging.cloud.ibm.com"))
+			Expect(locator.ICDEndpoint()).To(Equal("https://api.jp-tok.databases.cloud.ibm.com"))
+		})
+	})
+
+
+
 
 	Context("When region is not supported", func() {
 		locator := newEndpointLocator("in")
@@ -85,7 +117,25 @@ var _ = Describe("EndPoints", func() {
 			Expect(err).To(HaveOccurred())
 			_, err = locator.ContainerEndpoint()
 			Expect(err).To(HaveOccurred())
-
+			_, err = locator.MCCPAPIEndpoint()
+			Expect(err).To(HaveOccurred())
+			_, err = locator.CisEndpoint()
+			Expect(err).To(HaveOccurred())
+			_, err = locator.GlobalSearchEndpoint()
+			Expect(err).To(HaveOccurred())
+			_, err = locator.GlobalTaggingEndpoint()
+			Expect(err).To(HaveOccurred())
+			_, err = locator.IAMPAPEndpoint()
+			Expect(err).To(HaveOccurred())
+			_, err = locator.ICDEndpoint()
+			Expect(err).To(HaveOccurred())
+			_, err = locator.ResourceManagementEndpoint()
+			Expect(err).To(HaveOccurred())
+			_, err = locator.ResourceControllerEndpoint()
+			Expect(err).To(HaveOccurred())
+			_, err = locator.ResourceCatalogEndpoint()
+			Expect(err).To(HaveOccurred())
+			_, err = locator.ContainerRegistryEndpoint() 
 		})
 	})
 
