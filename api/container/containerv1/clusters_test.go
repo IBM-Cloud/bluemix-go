8f6bdba31b7e14d2243d491801fc0eb7276f4a7a
Praveen K. Gostu <praveek9@in.ibm.com>
2019-03-28 18:32:18 +0530

    Update cluster api with service endpoints

--- api/container/containerv1/clusters_test.go
+++ api/container/containerv1/clusters_test.go
@@ -27,7 +27,7 @@ var _ = Describe("Clusters", func() {
 				server.AppendHandlers(
 					ghttp.CombineHandlers(
 						ghttp.VerifyRequest(http.MethodPost, "/v1/clusters"),
-						ghttp.VerifyJSON(`{"dataCenter":"dal10","isolation":"","machineType":"b2c.4x16","name":"testservice","privateVlan":"vlan","publicVlan":"vlan","workerNum":1,"noSubnet":false,"masterVersion":"1.8.1","prefix":"worker","diskEncryption": true,"enableTrusted":true}
+						ghttp.VerifyJSON(`{"dataCenter":"dal10","isolation":"","machineType":"b2c.4x16","name":"testservice","privateVlan":"vlan","publicVlan":"vlan","workerNum":1,"noSubnet":false,"masterVersion":"1.8.1","prefix":"worker","diskEncryption": true,"enableTrusted":true,"privateSeviceEndpoint": false,"publicServiceEndpoint": false}
 `),
 						ghttp.RespondWith(http.StatusCreated, `{							 	
 							 "id": "f91adfe2-76c9-4649-939e-b01c37a3704c"
@@ -57,7 +57,7 @@ var _ = Describe("Clusters", func() {
 				server.AppendHandlers(
 					ghttp.CombineHandlers(
 						ghttp.VerifyRequest(http.MethodPost, "/v1/clusters"),
-						ghttp.VerifyJSON(`{"dataCenter":"dal10","isolation":"","machineType":"free","name":"testservice","privateVlan":"vlan","publicVlan":"vlan","workerNum":1,"noSubnet":false,"masterVersion":"1.8.1","prefix":"worker","diskEncryption": false,"enableTrusted":false}
+						ghttp.VerifyJSON(`{"dataCenter":"dal10","isolation":"","machineType":"free","name":"testservice","privateVlan":"vlan","publicVlan":"vlan","workerNum":1,"noSubnet":false,"masterVersion":"1.8.1","prefix":"worker","diskEncryption": false,"enableTrusted":false,"privateSeviceEndpoint": false,"publicServiceEndpoint": false}
 `),
 						ghttp.RespondWith(http.StatusInternalServerError, `Failed to create cluster`),
 					),
