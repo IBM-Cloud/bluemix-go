d70d317ad0ad6ec9c818aeb7e980c57fb8e5bffa
Anil <anil@Anils-MacBook-Pro.local>
2019-10-14 00:05:22 +0530

    unit test code

--- api/container/containerv2/clusters_test.go
+++ api/container/containerv2/clusters_test.go
@@ -94,7 +94,64 @@ var _ = Describe("Clusters", func() {
 		})
 	})
 
-	//
+	//Create
+	Describe("Create", func() {
+		Context("When creation is successful", func() {
+			BeforeEach(func() {
+				server = ghttp.NewServer()
+				server.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest(http.MethodPost, "/v2/vpc/createCluster"),
+						ghttp.VerifyJSON(`{"disablePublicServiceEndpoint": true, "name": "abcd", "podSubnet": "10.10.1.0", "provider": "abc", "serviceSubnet": "10.10.20.1", "workerPool": { "diskEncryption": true, "flavor": "b2c.4x16", "isolation": "", "name": "vpc-test", "vpcID": "vpc-test-123", "workerCount": 3, "zones": []}}`)
+						ghttp.RespondWith(http.StatusCreated, `{							 	
+							 "id": "f91adfe2-76c9-4649-939e-b01c37a3704c"
+						}`),
+					),
+				)
+			})
+
+			It("should return cluster created", func() {
+				params := ClusterCreateRequest{
+					Name: "testservice", PodSubnet: "10.10.1.0", Provider: "abc", ServiceSubnet: "10.10.20.1",
+				}
+				target := ClusterTargetHeader{
+					OrgID:     "abc",
+					SpaceID:   "def",
+					AccountID: "ghi",
+				}
+				myCluster, err := newCluster(server.URL()).Create(params, target)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(myCluster).ShouldNot(BeNil())
+				Expect(myCluster.ID).Should(Equal("f91adfe2-76c9-4649-939e-b01c37a3704c"))
+			})
+		})
+		Context("When creation is unsuccessful", func() {
+			BeforeEach(func() {
+				server = ghttp.NewServer()
+				server.SetAllowUnhandledRequests(true)
+				server.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest(http.MethodPost, "/v2/vpc/createCluster"),
+						ghttp.VerifyJSON(`{"disablePublicServiceEndpoint": true, "name": "abcd", "podSubnet": "10.10.1.0", "provider": "abc", "serviceSubnet": "10.10.20.1", "workerPool": { "diskEncryption": true, "flavor": "b2c.4x16", "isolation": "", "name": "vpc-test", "vpcID": "vpc-test-123", "workerCount": 3, "zones": []}}`),
+						ghttp.RespondWith(http.StatusInternalServerError, `Failed to create cluster`),
+					),
+				)
+			})
+			It("should return error during cluster creation", func() {
+				params := ClusterCreateRequest{
+					Name: "testservice", Datacenter: "dal10", MachineType: "free", PublicVlan: "vlan", PrivateVlan: "vlan", MasterVersion: "1.8.1", Prefix: "worker", WorkerNum: 1,
+				}
+				target := ClusterTargetHeader{
+					OrgID:     "abc",
+					SpaceID:   "def",
+					AccountID: "ghi",
+				}
+				myCluster, err := newCluster(server.URL()).Create(params, target)
+				Expect(err).To(HaveOccurred())
+				Expect(myCluster.ID).Should(Equal(""))
+			})
+		})
+	})
 })
 
 func newCluster(url string) Clusters {
