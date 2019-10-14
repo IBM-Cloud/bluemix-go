dce91a81e4d6d81110902dc59056ad89b848666d
Anil <anil@Anils-MacBook-Pro.local>
2019-10-14 17:57:49 +0530

    code cleanup

--- examples/container/v2Containers/getClusterInfoV2/main.go
+++ examples/container/v2Containers/getClusterInfoV2/main.go
@@ -7,7 +7,6 @@ import (
 
 	bluemix "github.com/IBM-Cloud/bluemix-go"
 
-	//v1 "github.com/IBM-Cloud/bluemix-go/api/container/containerv1"
 	v2 "github.com/IBM-Cloud/bluemix-go/api/container/containerv2"
 	"github.com/IBM-Cloud/bluemix-go/session"
 
@@ -20,35 +19,15 @@ func main() {
 
 	var zone string
 	flag.StringVar(&zone, "zone", "us-south-1", "Zone")
-
-	// var privateVlan string
-	// flag.StringVar(&privateVlan, "privateVlan", "", "Zone Private Vlan")
-
-	// var publicVlan string
-	// flag.StringVar(&publicVlan, "publicVlan", "", "Zone Public vlan")
-
-	// var updatePrivateVlan string
-	// flag.StringVar(&updatePrivateVlan, "updatePrivateVlan", "", "Zone Private vlan to be updated")
-
-	// var updatePublicVlan string
-	// flag.StringVar(&updatePublicVlan, "updatePublicVlan", "", "Zone Public vlan to be updated")
-
 	var location string
 	flag.StringVar(&location, "location", "dallas", "location")
 
 	var region string
 	flag.StringVar(&location, "region", "us-south", "region")
 
-	// var skipDeletion bool
-	// flag.BoolVar(&skipDeletion, "no-delete", false, "If provided will delete the resources created")
-
 	flag.Parse()
 
 	trace.Logger = trace.NewLogger("true")
-	// if privateVlan == "" || publicVlan == "" || updatePrivateVlan == "" || updatePublicVlan == "" || zone == "" || location == "" {
-	// 	flag.Usage()
-	// 	os.Exit(1)
-	// }
 
 	target := v2.ClusterTargetHeader{}
 
