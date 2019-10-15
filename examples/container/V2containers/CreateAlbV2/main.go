42cc8fe124a7d421f27e8ba5d360d1a4b4a52a9c
smjtd <55692060+smjtd@users.noreply.github.com>
2019-10-15 14:58:09 +0530

    Add support for vpc cluster sdk and examples (#160)
    * vpc cluster sdk and examples added
    
    * list removed

--- examples/container/V2containers/CreateAlbV2/main.go
+++ examples/container/V2containers/CreateAlbV2/main.go
@@ -1,23 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 
 	bluemix "github.com/IBM-Cloud/bluemix-go"
 	"github.com/IBM-Cloud/bluemix-go/session"
-
-	v1 "github.com/IBM-Cloud/bluemix-go/api/container/containerv1"
-	v2 "github.com/IBM-Cloud/bluemix-go/api/container/containerv2"
 	"github.com/IBM-Cloud/bluemix-go/trace"
+
+	v2 "github.com/IBM-Cloud/bluemix-go/api/container/containerv2"
 )
 
 func main() {
 
 	c := new(bluemix.Config)
 
+	var zone string
+	flag.StringVar(&zone, "zone", "us-south-1", "Zone")
+	flag.Parse()
+
 	trace.Logger = trace.NewLogger("true")
 
+	var albinfo = v2.AlbCreateReq{
+		Cluster:         "bm64u3ed02o93vv36hb0",
+		EnableByDefault: true,
+		Type:            "private",
+		ZoneAlb:         "us-south-1",
+	}
 	sess, err := session.New(c)
 	if err != nil {
 		log.Fatal(err)
@@ -26,19 +36,17 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	target := v1.ClusterTargetHeader{}
+	target := v2.ClusterTargetHeader{}
 
 	clusterClient, err := v2.New(sess)
 	if err != nil {
 		log.Fatal(err)
 	}
-	clustersAPI := clusterClient.Clusters()
 
-	out, err := clustersAPI.List(target)
-	if err != nil {
-		log.Fatal(err)
-	}
+	albAPI := clusterClient.Albs()
 
-	fmt.Println("Clusters ", out)
+	err2 := albAPI.CreateAlb(albinfo, target)
+
+	fmt.Println("err=", err2)
+
 }
--- examples/container/list/README.md
+++
@@ -1,9 +0,0 @@
-# Cluster example
-
-This example shows how to retrive the list of VPC clusters
-
-Example:
-
-```
-go run main.go
-```
