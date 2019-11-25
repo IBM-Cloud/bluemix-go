3fc387ce25383eb4e726718207c9b4fd87c6f2bc
Soumyajit <soumyda1@in.ibm.com>
2019-11-25 10:58:30 +0530

    added create workspace skd and bug fix

--- examples/schematics/main.go
+++ examples/schematics/main.go
@@ -15,9 +15,46 @@ import (
 func main() {
 	c := new(bluemix.Config)
 
-	workspaceID := "myworkspaceptab-bc54176d-bbcb-42"
-	templateid := "29291199-ca08-46"
+	var workspaceID string
+	var templateid string
+	fmt.Print("Enter Workspace ID: ")
+	fmt.Scanln(&workspaceID)
+	fmt.Print("Enter Template ID: ")
+	fmt.Scanln(&templateid)
+
 	trace.Logger = trace.NewLogger("true")
+	var payload = sch.Payload{
+		Name:        "bbb",
+		Type:        []string{"terraform-v1.0"},
+		Description: "terraform workspace",
+		Tags:        []string{"department:HR", "application:compensation", "environment:staging"},
+		WorkspaceStatus: sch.WorkspaceStatus{
+			Frozen: true,
+		},
+		TemplateRepo: sch.TemplateRepo{
+			URL: "https://github.com/ptaube/tf_cloudless_sleepy",
+		},
+		TemplateRef: "ibm-open-liberty-2ae855ce3ca4",
+		TemplateData: []sch.TemplateData{
+			{
+				Folder: ".",
+				Type:   "terraform-v1.0",
+
+				Variablestore: []sch.Variablestore{
+					{
+						Name:        "sample_var",
+						Secure:      false,
+						Value:       "THIS IS IBM CLOUD TERRAFORM CLI DEMO",
+						Description: "Description of sample_var",
+					},
+					{
+						Name:  "sleepy_time",
+						Value: "15",
+					},
+				},
+			},
+		},
+	}
 
 	sess, err := session.New(c)
 	if err != nil {
@@ -49,4 +86,27 @@ func main() {
 	}
 
 	fmt.Println("The output info= ", out)
+	items := make(map[string]interface{})
+
+	for _, feilds := range out {
+		if feilds.TemplateID == "653f60a4-f64f-41" {
+			output := feilds.Output
+
+			for _, value := range output {
+				for key, val := range value {
+					val2 := val.Value
+					items[key] = val2
+
+				}
+			}
+		}
+	}
+
+	fmt.Println("\nThe array info= ", items)
+
+	cdata, err := schAPI.CreateWorkspace(payload)
+
+	fmt.Println("\ndata=", cdata)
+	fmt.Println("\nid=", cdata.ID)
+
 }
