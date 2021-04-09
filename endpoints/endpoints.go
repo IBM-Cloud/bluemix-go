25fe6f0419544558a000a180a5bf3d968fe9c7c3
Marco Bulgarini <marco.bulgarini@sysdig.com>
2021-04-09 12:12:33 +0200

    Add ICD endpoint for Toronto

--- endpoints/endpoints.go
+++ endpoints/endpoints.go
@@ -67,7 +67,7 @@ var regionToEndpoint = map[string]map[string]string{
 var privateRegions = map[string][]string{
 	"accounts":              {"us-south", "us-east"},
 	"certificate-manager":   {"us-south", "us-east", "eu-gb", "eu-de", "jp-tok", "au-syd"},
-	"icd":                   {"us-south", "us-east", "eu-gb", "eu-de", "jp-tok", "au-syd", "osl01", "seo01", "che01"},
+	"icd":                   {"us-south", "us-east", "eu-gb", "eu-de", "jp-tok", "au-syd", "osl01", "seo01", "che01", "ca-tor"},
 	"schematics":            {"us-south", "us-east", "eu-de", "eu-gb"},
 	"global-search-tagging": {"us-south", "us-east"},
 	"container":             {"us-south", "us-east", "eu-gb", "eu-de", "jp-tok", "au-syd", "jp-osa", "ca-tor"},
