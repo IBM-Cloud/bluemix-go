1e1cd1634f9161becb391b7001d8495197e809ae
Zoltan Illes <zoltan.illes@ibm.com>
2023-04-21 14:38:08 +0200

    containerv1 getworkerpool example

---
+++ examples/container/getWorkerPool/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	bluemix "github.com/IBM-Cloud/bluemix-go"
+	"github.com/IBM-Cloud/bluemix-go/session"
+
+	v1 "github.com/IBM-Cloud/bluemix-go/api/container/containerv1"
+	"github.com/IBM-Cloud/bluemix-go/trace"
+)
+
+func main() {
+
+	c := new(bluemix.Config)
+
+	var ResourceGroup string
+	flag.StringVar(&ResourceGroup, "resourcegroup", "", "ResourceGroup")
+
+	var Region string
+	flag.StringVar(&Region, "region", "", "Region")
+
+	var Cluster string
+	flag.StringVar(&Cluster, "cluster", "", "Cluster")
+
+	var WorkerPool string
+	flag.StringVar(&WorkerPool, "workerpool", "", "WorkerPool")
+
+	flag.Parse()
+
+	trace.Logger = trace.NewLogger("true")
+
+	sess, err := session.New(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	target := v1.ClusterTargetHeader{}
+	target.Region = Region
+	target.ResourceGroup = ResourceGroup
+
+	clusterClient, err := v1.New(sess)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	workerPoolAPI := clusterClient.WorkerPools()
+
+	pool, err := workerPoolAPI.GetWorkerPool(Cluster, WorkerPool, target)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("WorkerPool get was successful\n %v", pool)
+}
