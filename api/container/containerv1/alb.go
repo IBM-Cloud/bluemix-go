84239f5f7178f0d5a4d06e81f1762c01bc96107a
Sandor Szombat <sandor.szombat@ibm.com>
2023-10-11 11:31:31 +0000

    Review fixes

--- api/container/containerv1/alb.go
+++ api/container/containerv1/alb.go
@@ -6,16 +6,6 @@ import (
 	"github.com/IBM-Cloud/bluemix-go/client"
 )
 
-type ClusterALB struct {
-	ID                string      `json:"id"`
-	Region            string      `json:"region"`
-	DataCenter        string      `json:"dataCenter"`
-	IsPaid            bool        `json:"isPaid"`
-	IngressHostname   string      `json:"ingressHostname"`
-	IngressSecretName string      `json:"ingressSecretName"`
-	ALBs              []ALBConfig `json:"alb"`
-}
-
 // ALBConfig config for alb configuration
 // swagger:model
 type ALBConfig struct {
@@ -33,14 +23,10 @@ type ALBConfig struct {
 	DisableDeployment bool   `json:"disableDeployment" description:"Indicate whether to disable deployment only on disable alb"`
 }
 
-// ClusterALBSecret albsecret related information for cluster
-// swagger:model
-type ClusterALBSecret struct {
-	ID         string            `json:"id"`
-	Region     string            `json:"region"`
-	DataCenter string            `json:"dataCenter"`
-	IsPaid     bool              `json:"isPaid"`
-	ALBSecrets []ALBSecretConfig `json:"albSecrets" description:"All the ALB secrets created in this cluster"`
+// AlbCreateResp used to send back the albid on a create request
+type AlbCreateResp struct {
+	Alb     string `json:"alb"`
+	Cluster string `json:"cluster"`
 }
 
 // ALBSecretConfig config for alb-secret configuration
@@ -62,6 +48,32 @@ type ALBSecretsPerCRN struct {
 	ALBSecrets []string `json:"albsecrets" description:"ALB secrets correponding to a CRN"`
 }
 
+// ALBUpdatePolicy represents the body for interacting with the update ALB APIs.
+type ALBUpdatePolicy struct {
+	AutoUpdate    bool `json:"autoUpdate" binding:"required"`
+	LatestVersion bool `json:"latestVersion"`
+}
+
+type ClusterALB struct {
+	ID                string      `json:"id"`
+	Region            string      `json:"region"`
+	DataCenter        string      `json:"dataCenter"`
+	IsPaid            bool        `json:"isPaid"`
+	IngressHostname   string      `json:"ingressHostname"`
+	IngressSecretName string      `json:"ingressSecretName"`
+	ALBs              []ALBConfig `json:"alb"`
+}
+
+// ClusterALBSecret albsecret related information for cluster
+// swagger:model
+type ClusterALBSecret struct {
+	ID         string            `json:"id"`
+	Region     string            `json:"region"`
+	DataCenter string            `json:"dataCenter"`
+	IsPaid     bool              `json:"isPaid"`
+	ALBSecrets []ALBSecretConfig `json:"albSecrets" description:"All the ALB secrets created in this cluster"`
+}
+
 // CreateALB struct representation of ALB arriving in create requests
 type CreateALB struct {
 	Zone            string `json:"zone"`
@@ -73,38 +85,26 @@ type CreateALB struct {
 	IngressImage    string `json:"ingressImage,omitempty"`
 }
 
-// AlbCreateResp used to send back the albid on a create request
-type AlbCreateResp struct {
-	Alb     string `json:"alb"`
-	Cluster string `json:"cluster"`
-}
-
-// ALBUpdatePolicy represents the body for interacting with the update ALB APIs.
-type ALBUpdatePolicy struct {
-	AutoUpdate    bool `json:"autoUpdate" binding:"required"`
-	LatestVersion bool `json:"latestVersion"`
-}
-
 // Clusters interface
 type Albs interface {
-	CreateALB(alb CreateALB, clusterID string, target ClusterTargetHeader) (AlbCreateResp, error)
-	ListClusterALBs(clusterNameOrID string, target ClusterTargetHeader) ([]ALBConfig, error)
-	GetALB(albID string, target ClusterTargetHeader) (ALBConfig, error)
+	ChangeALBUpdatePolicy(clusterID string, config ALBUpdatePolicy, target ClusterTargetHeader) error
 	ConfigureALB(albID string, config ALBConfig, disableDeployment bool, target ClusterTargetHeader) error
-	RemoveALB(albID string, target ClusterTargetHeader) error
+	CreateALB(alb CreateALB, clusterID string, target ClusterTargetHeader) (AlbCreateResp, error)
 	DeployALBCert(config ALBSecretConfig, target ClusterTargetHeader) error
-	UpdateALBCert(config ALBSecretConfig, target ClusterTargetHeader) error
-	RemoveALBCertBySecretName(clusterID string, secretName string, target ClusterTargetHeader) error
-	RemoveALBCertByCertCRN(clusterID string, certCRN string, target ClusterTargetHeader) error
-	GetClusterALBCertBySecretName(clusterID string, secretName string, target ClusterTargetHeader) (ALBSecretConfig, error)
-	GetClusterALBCertByCertCRN(clusterID string, certCRN string, target ClusterTargetHeader) (ALBSecretConfig, error)
-	ListALBCerts(clusterID string, target ClusterTargetHeader) ([]ALBSecretConfig, error)
+	DisableALB(albID string, target ClusterTargetHeader) error
+	EnableALB(albID string, config ALBConfig, target ClusterTargetHeader) error
+	GetALB(albID string, target ClusterTargetHeader) (ALBConfig, error)
 	GetALBTypes(target ClusterTargetHeader) ([]string, error)
 	GetALBUpdatePolicy(clusterID string, target ClusterTargetHeader) (ALBUpdatePolicy, error)
-	ChangeALBUpdatePolicy(clusterID string, config ALBUpdatePolicy, target ClusterTargetHeader) error
+	GetClusterALBCertByCertCRN(clusterID string, certCRN string, target ClusterTargetHeader) (ALBSecretConfig, error)
+	GetClusterALBCertBySecretName(clusterID string, secretName string, target ClusterTargetHeader) (ALBSecretConfig, error)
+	ListALBCerts(clusterID string, target ClusterTargetHeader) ([]ALBSecretConfig, error)
+	ListClusterALBs(clusterNameOrID string, target ClusterTargetHeader) ([]ALBConfig, error)
+	RemoveALB(albID string, target ClusterTargetHeader) error
+	RemoveALBCertByCertCRN(clusterID string, certCRN string, target ClusterTargetHeader) error
+	RemoveALBCertBySecretName(clusterID string, secretName string, target ClusterTargetHeader) error
+	UpdateALBCert(config ALBSecretConfig, target ClusterTargetHeader) error
 	UpdateALBs(clusterID string, target ClusterTargetHeader) error
-	EnableALB(albID string, config ALBConfig, target ClusterTargetHeader) error
-	DisableALB(albID string, target ClusterTargetHeader) error
 }
 
 type alb struct {
--- api/container/containerv2/alb.go
+++ api/container/containerv2/alb.go
@@ -6,28 +6,10 @@ import (
 	"github.com/IBM-Cloud/bluemix-go/client"
 )
 
-type AlbCreateReq struct {
-	Cluster         string `json:"cluster"`
-	EnableByDefault bool   `json:"enableByDefault"`
-	Type            string `json:"type"`
-	ZoneAlb         string `json:"zone"`
-	IngressImage    string `json:"ingressImage"`
+type alb struct {
+	client *client.Client
 }
 
-type AlbCreateResp struct {
-	Cluster string `json:"cluster"`
-	Alb     string `json:"alb"`
-}
-
-type ClusterALB struct {
-	ID                      string      `json:"id"`
-	Region                  string      `json:"region"`
-	DataCenter              string      `json:"dataCenter"`
-	IsPaid                  bool        `json:"isPaid"`
-	PublicIngressHostname   string      `json:"publicIngressHostname"`
-	PublicIngressSecretName string      `json:"publicIngressSecretName"`
-	ALBs                    []AlbConfig `json:"alb"`
-}
 type AlbConfig struct {
 	AlbBuild             string `json:"albBuild"`
 	AlbID                string `json:"albID"`
@@ -46,11 +28,42 @@ type AlbConfig struct {
 	ZoneAlb              string `json:"zone"`
 }
 
-// UpdateALBReq is the body of the v2 Update ALB API endpoint
-type UpdateALBReq struct {
-	ClusterID string   `json:"cluster" description:"The ID of the cluster on which the update ALB action shall be performed"`
-	ALBBuild  string   `json:"albBuild" description:"The version of the build to which the ALB should be updated"`
-	ALBList   []string `json:"albList" description:"The list of ALBs that should be updated to the requested albBuild"`
+// ALBClusterHealthCheckConfig configuration for ALB in-cluster health check
+type ALBClusterHealthCheckConfig struct {
+	Cluster string `json:"cluster"`
+	Enable  bool   `json:"enable"`
+}
+
+type AlbCreateResp struct {
+	Cluster string `json:"cluster"`
+	Alb     string `json:"alb"`
+}
+
+type AlbCreateReq struct {
+	Cluster         string `json:"cluster"`
+	EnableByDefault bool   `json:"enableByDefault"`
+	Type            string `json:"type"`
+	ZoneAlb         string `json:"zone"`
+	IngressImage    string `json:"ingressImage"`
+}
+
+type AlbImageVersions struct {
+	DefaultK8sVersion    string   `json:"defaultK8sVersion"`
+	SupportedK8sVersions []string `json:"supportedK8sVersions"`
+}
+
+// ALBLBConfig ingress ALB load balancer configuration
+type ALBLBConfig struct {
+	Cluster       string                    `json:"cluster" description:"The ID or name of the cluster" binding:"required"`
+	Type          string                    `json:"type,omitempty" description:"type of load balancers to configure ('public' or 'private')"`
+	ProxyProtocol *ALBLBProxyProtocolConfig `json:"proxyProtocol,omitempty" description:"PROXY protocol related configurations"`
+}
+
+// ALBLBProxyProtocolConfig ingress ALB load balancer Proxy Protocol configuration
+type ALBLBProxyProtocolConfig struct {
+	Enable        bool     `json:"enable" description:"PROXY protocol state"`
+	CIDR          []string `json:"cidr,omitempty" description:"trusted address ranges"`
+	HeaderTimeout int      `json:"headerTimeout,omitempty" description:"timeout value for receiving PROXY protocol headers"`
 }
 
 // AlbUpdateResp
@@ -58,9 +71,39 @@ type AlbUpdateResp struct {
 	ClusterID string `json:"clusterID"`
 }
 
-type AlbImageVersions struct {
-	DefaultK8sVersion    string   `json:"defaultK8sVersion"`
-	SupportedK8sVersions []string `json:"supportedK8sVersions"`
+// AutoscaleDetails
+type AutoscaleDetails struct {
+	Config *AutoscaleConfig `json:"config,omitempty" description:"Autoscaling configuration"`
+}
+
+// AutoscaleConfig
+type AutoscaleConfig struct {
+	MinReplicas           int    `json:"minReplicas" description:"Minimum number of replicas"`
+	MaxReplicas           int    `json:"maxReplicas" description:"Maximum number of replicas"`
+	CustomMetrics         string `json:"customMetrics,omitempty" description:"An array of MetricSpec (autoscaling.k8s.io/v2) encoded as JSON"`
+	CPUAverageUtilization int    `json:"cpuAverageUtilization,omitempty" description:"CPU Average Utilization"`
+}
+
+type ClusterALB struct {
+	ID                      string      `json:"id"`
+	Region                  string      `json:"region"`
+	DataCenter              string      `json:"dataCenter"`
+	IsPaid                  bool        `json:"isPaid"`
+	PublicIngressHostname   string      `json:"publicIngressHostname"`
+	PublicIngressSecretName string      `json:"publicIngressSecretName"`
+	ALBs                    []AlbConfig `json:"alb"`
+}
+
+// IgnoredIngressStatusErrors
+type IgnoredIngressStatusErrors struct {
+	Cluster       string   `json:"cluster" description:"the ID or name of the cluster"`
+	IgnoredErrors []string `json:"ignoredErrors" description:"list of error codes that the user wants to ignore"`
+}
+
+// IngressStatusState
+type IngressStatusState struct {
+	Cluster string `json:"cluster" description:"the ID or name of the cluster"`
+	Enable  bool   `json:"enable" description:"true or false to enable or disable ingress status"`
 }
 
 // IngressStatus struct for the top level ingress status, for a cluster
@@ -85,82 +128,40 @@ type IngressComponentStatus struct {
 	Type      string `json:"type"`
 }
 
+// UpdateALBReq is the body of the v2 Update ALB API endpoint
+type UpdateALBReq struct {
+	ClusterID string   `json:"cluster" description:"The ID of the cluster on which the update ALB action shall be performed"`
+	ALBBuild  string   `json:"albBuild" description:"The version of the build to which the ALB should be updated"`
+	ALBList   []string `json:"albList" description:"The list of ALBs that should be updated to the requested albBuild"`
+}
+
 // V2IngressComponentStatus status of individual ingress component
 type V2IngressComponentStatus struct {
 	Component string   `json:"component,omitempty"`
 	Status    []string `json:"status,omitempty"`
 }
 
-// ALBClusterHealthCheckConfig configuration for ALB in-cluster health check
-type ALBClusterHealthCheckConfig struct {
-	Cluster string `json:"cluster"`
-	Enable  bool   `json:"enable"`
-}
-
-// IgnoredIngressStatusErrors
-type IgnoredIngressStatusErrors struct {
-	Cluster       string   `json:"cluster" description:"the ID or name of the cluster"`
-	IgnoredErrors []string `json:"ignoredErrors" description:"list of error codes that the user wants to ignore"`
-}
-
-// IngressStatusState
-type IngressStatusState struct {
-	Cluster string `json:"cluster" description:"the ID or name of the cluster"`
-	Enable  bool   `json:"enable" description:"true or false to enable or disable ingress status"`
-}
-
-// ALBLBConfig ingress ALB load balancer configuration
-type ALBLBConfig struct {
-	Cluster       string                    `json:"cluster" description:"The ID or name of the cluster" binding:"required"`
-	Type          string                    `json:"type,omitempty" description:"type of load balancers to configure ('public' or 'private')"`
-	ProxyProtocol *ALBLBProxyProtocolConfig `json:"proxyProtocol,omitempty" description:"PROXY protocol related configurations"`
-}
-
-// ALBLBProxyProtocolConfig ingress ALB load balancer Proxy Protocol configuration
-type ALBLBProxyProtocolConfig struct {
-	Enable        bool     `json:"enable" description:"PROXY protocol state"`
-	CIDR          []string `json:"cidr,omitempty" description:"trusted address ranges"`
-	HeaderTimeout int      `json:"headerTimeout,omitempty" description:"timeout value for receiving PROXY protocol headers"`
-}
-
-// AutoscaleDetails
-type AutoscaleDetails struct {
-	Config *AutoscaleConfig `json:"config,omitempty" description:"Autoscaling configuration"`
-}
-
-// AutoscaleConfig
-type AutoscaleConfig struct {
-	MinReplicas           int    `json:"minReplicas" description:"Minimum number of replicas"`
-	MaxReplicas           int    `json:"maxReplicas" description:"Maximum number of replicas"`
-	CustomMetrics         string `json:"customMetrics,omitempty" description:"An array of MetricSpec (autoscaling.k8s.io/v2) encoded as JSON"`
-	CPUAverageUtilization int    `json:"cpuAverageUtilization,omitempty" description:"CPU Average Utilization"`
-}
-
-type alb struct {
-	client *client.Client
-}
-
 // Clusters interface
 type Alb interface {
+	AddIgnoredIngressStatusErrors(ignoredErrorsReq IgnoredIngressStatusErrors, target ClusterTargetHeader) error
 	CreateAlb(albCreateReq AlbCreateReq, target ClusterTargetHeader) (AlbCreateResp, error)
 	DisableAlb(disableAlbReq AlbConfig, target ClusterTargetHeader) error
 	EnableAlb(enableAlbReq AlbConfig, target ClusterTargetHeader) error
-	UpdateAlb(updateAlbReq UpdateALBReq, target ClusterTargetHeader) error
-	GetAlb(albid string, target ClusterTargetHeader) (AlbConfig, error)
-	ListClusterAlbs(clusterNameOrID string, target ClusterTargetHeader) ([]AlbConfig, error)
-	ListAlbImages(target ClusterTargetHeader) (AlbImageVersions, error)
-	GetIngressStatus(clusterNameOrID string, target ClusterTargetHeader) (IngressStatus, error)
-	GetAlbClusterHealthCheckConfig(clusterNameOrID string, target ClusterTargetHeader) (ALBClusterHealthCheckConfig, error)
-	SetAlbClusterHealthCheckConfig(albHealthCheckReq ALBClusterHealthCheckConfig, target ClusterTargetHeader) error
-	GetIgnoredIngressStatusErrors(clusterNameOrID string, target ClusterTargetHeader) (IgnoredIngressStatusErrors, error)
-	AddIgnoredIngressStatusErrors(ignoredErrorsReq IgnoredIngressStatusErrors, target ClusterTargetHeader) error
-	RemoveIgnoredIngressStatusErrors(ignoredErrorsReq IgnoredIngressStatusErrors, target ClusterTargetHeader) error
-	SetIngressStatusState(ingressStatusStateReq IngressStatusState, target ClusterTargetHeader) error
-	GetIngressLoadBalancerConfig(clusterNameOrID, lbType string, target ClusterTargetHeader) (ALBLBConfig, error)
-	UpdateIngressLoadBalancerConfig(lbConfig ALBLBConfig, target ClusterTargetHeader) error
 	GetALBAutoscaleConfiguration(clusterNameOrID, albID string, target ClusterTargetHeader) (AutoscaleDetails, error)
-	SetALBAutoscaleConfiguration(clusterNameOrID, albID string, autoscaleDetails AutoscaleDetails, target ClusterTargetHeader) error
+	GetAlb(albid string, target ClusterTargetHeader) (AlbConfig, error)
+	GetAlbClusterHealthCheckConfig(clusterNameOrID string, target ClusterTargetHeader) (ALBClusterHealthCheckConfig, error)
+	GetIgnoredIngressStatusErrors(clusterNameOrID string, target ClusterTargetHeader) (IgnoredIngressStatusErrors, error)
+	GetIngressLoadBalancerConfig(clusterNameOrID, lbType string, target ClusterTargetHeader) (ALBLBConfig, error)
+	GetIngressStatus(clusterNameOrID string, target ClusterTargetHeader) (IngressStatus, error)
+	ListAlbImages(target ClusterTargetHeader) (AlbImageVersions, error)
+	ListClusterAlbs(clusterNameOrID string, target ClusterTargetHeader) ([]AlbConfig, error)
 	RemoveALBAutoscaleConfiguration(clusterNameOrID, albID string, target ClusterTargetHeader) error
+	RemoveIgnoredIngressStatusErrors(ignoredErrorsReq IgnoredIngressStatusErrors, target ClusterTargetHeader) error
+	SetALBAutoscaleConfiguration(clusterNameOrID, albID string, autoscaleDetails AutoscaleDetails, target ClusterTargetHeader) error
+	SetAlbClusterHealthCheckConfig(albHealthCheckReq ALBClusterHealthCheckConfig, target ClusterTargetHeader) error
+	SetIngressStatusState(ingressStatusStateReq IngressStatusState, target ClusterTargetHeader) error
+	UpdateAlb(updateAlbReq UpdateALBReq, target ClusterTargetHeader) error
+	UpdateIngressLoadBalancerConfig(lbConfig ALBLBConfig, target ClusterTargetHeader) error
 }
 
 func newAlbAPI(c *client.Client) Alb {
